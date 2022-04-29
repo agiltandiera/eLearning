ede7773de2b4f8ff2b53bcd1ecc162fa0f82d75e
Agil Tandiera <60023592+agiltandiera@users.noreply.github.com>
2022-04-29 17:24:40 +0800

    create packages

--- app/src/main/java/com/tandiera/project/elearning/presentation/main/MainActivity.kt
+++ app/src/main/java/com/tandiera/project/elearning/presentation/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.tandiera.project.elearning
+package com.tandiera.project.elearning.presentation.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tandiera.project.elearning.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
