c183d34dd95ba767430bbef9721389d70ca88f5a
Agil Tandiera <60023592+agiltandiera@users.noreply.github.com>
2022-05-11 15:18:00 +0800

    show user data from firebase

--- app/src/main/java/com/tandiera/project/elearning/presentation/user/UserActivity.kt
+++ app/src/main/java/com/tandiera/project/elearning/presentation/user/UserActivity.kt
@@ -4,16 +4,47 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings.ACTION_LOCALE_SETTINGS
+import android.util.Log
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
 import com.tandiera.project.elearning.databinding.ActivityUserBinding
+import com.tandiera.project.elearning.model.User
 import com.tandiera.project.elearning.presentation.changepassword.ChangePasswordActivity
 import com.tandiera.project.elearning.presentation.login.LoginActivity
+import com.tandiera.project.elearning.utils.showDialogError
 import org.jetbrains.anko.startActivity
 
 class UserActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityUserBinding
     private lateinit var firebaseAuth : FirebaseAuth
+    private lateinit var userDatasbase : DatabaseReference
+    private var currentUser : FirebaseUser? = null
+
+    private var listenerUser = object : ValueEventListener {
+        override fun onDataChange(snapshot: DataSnapshot) {
+            hideLoading()
+            val user = snapshot.getValue(User::class.java)
+            user?.let {
+                binding.tvNameUser.text = it.nameUser
+                binding.tvEmailUser.text = it.emailUser
+
+                Glide
+                    .with(this@UserActivity)
+                    .load(it.avatarUser)
+                    .placeholder(android.R.color.darker_gray)
+                    .into(binding.ivAvatarUser)
+            }
+        }
+
+        override fun onCancelled(error: DatabaseError) {
+            hideLoading()
+            showDialogError(this@UserActivity, error.message)
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +53,10 @@ class UserActivity : AppCompatActivity() {
 
         // Init
         firebaseAuth = FirebaseAuth.getInstance()
+        userDatasbase = FirebaseDatabase.getInstance().getReference("users")
+        currentUser = firebaseAuth.currentUser
+
+        getDataFirebase()
         onAction()
     }
 
@@ -44,6 +79,25 @@ class UserActivity : AppCompatActivity() {
                 startActivity<LoginActivity>()
                 finishAffinity()
             }
+
+            swipeUser.setOnRefreshListener {
+                getDataFirebase()
+            }
         }
+    }
+
+    private fun getDataFirebase() {
+        showLoading()
+        userDatasbase
+            .child(currentUser?.uid.toString())
+            .addValueEventListener(listenerUser)
+    }
+
+    private fun showLoading() {
+        binding.swipeUser.isRefreshing = true
+    }
+
+    private fun hideLoading() {
+        binding.swipeUser.isRefreshing = false
     }
 }
